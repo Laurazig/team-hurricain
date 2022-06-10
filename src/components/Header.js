19fb4f683dac5bd4b2076101d8ef66dfbe0b8645
Thanh Le <99728087+LeNganThanh@users.noreply.github.com>
2022-06-10 13:20:01 +0200

    changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,61 +1,58 @@
-import React, {useState} from "react";
-import "./Header.css"
+import React, { useState } from "react";
+import "./Header.css";
 
-const Header = ()=> {
-    const [ menuOpen, setMenuOpen ] = useState(false);
-    const onMenuClick = () => {
-        setMenuOpen(!menuOpen)
-    }
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const onMenuClick = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-
- 
-
-        
-
-return(
+  return (
     <div>
-         <header className={menuOpen?"nav-open": ""}>
-                <div className="topnav" id="myTopnav">
-                <a href="#home" className="active">Home</a>
-                <div className="nav-toggle">
-                <button onClick={onMenuClick} className="hamburger"></button>
-                </div>
+      <header className={menuOpen ? "nav-open" : ""}>
+        <div className="topnav" id="myTopnav">
+          <a href="#home" className="active">
+            Home
+          </a>
+          <div className="nav-toggle">
+            <button onClick={onMenuClick} className="hamburger"></button>
+          </div>
 
-                <a href="#news">News</a>
-                <a href="contact_form.html">Contact</a>
-                <div className="dropdown">
-                <button className="dropbtn">Teams</button>
-                <div className="dropdown-content">
-                    <a
-                    href="https://www.linkedin.com/in/feichengmarketing/"
-                    target="_blank"
-                    >
-                    Fei Cheng</a
-                    >
-                    <a
-                    href="https://www.linkedin.com/in/maria-papadaki74/"
-                    target="_blank"
-                    >
-                    Maria Papadaki</a
-                    >
-                    <a
-                    href="https://www.linkedin.com/in/thanh-le-249415240/"
-                    target="_blank"
-                    >
-                    Thanh Le</a
-                    >
-                    <a
-                    href="https://www.linkedin.com/in/laura-hesse-a998048a/"
-                    target="_blank"
-                    >
-                    Laura Hesse</a
-                    >
-                </div>
-                </div>
-                <a href="about.html">About</a>
+          <a href="#news">News</a>
+          <a href="#">Contact</a>
+          <div className="dropdown">
+            <button className="dropbtn">Teams</button>
+            <div className="dropdown-content">
+              <a
+                href="https://www.linkedin.com/in/feichengmarketing/"
+                target="_blank"
+              >
+                Fei Cheng
+              </a>
+              <a
+                href="https://www.linkedin.com/in/maria-papadaki74/"
+                target="_blank"
+              >
+                Maria Papadaki
+              </a>
+              <a
+                href="https://www.linkedin.com/in/thanh-le-249415240/"
+                target="_blank"
+              >
+                Thanh Le
+              </a>
+              <a
+                href="https://www.linkedin.com/in/laura-hesse-a998048a/"
+                target="_blank"
+              >
+                Laura Hesse
+              </a>
             </div>
-        </header>
+          </div>
+          <a href="about.html">About</a>
+        </div>
+      </header>
     </div>
-)
-}
-export default Header
+  );
+};
+export default Header;
